c3389375f2d837958384d4626edae46da7d1322d
abakarovmaks <abakarovmaks@gmail.com>
2020-11-13 10:09:16 +0200

    Создаёт функцию fetchCountries

--- goit-js-hw-12-countries/src/js/fetchCountries.js
+++ goit-js-hw-12-countries/src/js/fetchCountries.js
@@ -1,5 +1,5 @@
-export default fetchCountries(searchQuery) {
-    return new Promise(() => {
-        
-    })
+export function fetchCountries(searchQuery) {
+  return fetch(
+    `https://restcountries.eu/rest/v2/name/${searchQuery}`,
+  ).then(response => response.json());
 }
